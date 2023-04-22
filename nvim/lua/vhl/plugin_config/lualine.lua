e037ca0aabaae4c2dfe936ee5a424a501593a8ef
vuhuulan <vuhuulan18@gmail.com>
2023-04-22 09:00:45 +0900

    nvim: Edit lualine settings for no nerdfont

--- nvim/lua/vhl/plugin_config/lualine.lua
+++ nvim/lua/vhl/plugin_config/lualine.lua
@@ -1,6 +1,10 @@
+vim.opt.showmode = false        --no mode in cmd line bc statusline redundancy
+
 require('lualine').setup{
     options = {
         icons_enabled = false,
         theme = 'gruvbox',
+        component_separators = '|',
+        section_separators = '',
     },
 }
