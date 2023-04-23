29ee58e143de6c7654b6702b2be47f9493b8d5f4
vuhuulan <vuhuulan18@gmail.com>
2023-04-23 18:18:38 +0900

    nvim: Add SignColumn and remove its bg

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,3 @@
-require("vhl.set")
 require("vhl.plugins")
 require("vhl.plugin_config")
+require("vhl.set")
--- nvim/lua/vhl/set.lua
+++ nvim/lua/vhl/set.lua
@@ -1,5 +1,4 @@
 vim.opt.guicursor = ""
-
 vim.opt.mouse = ""
 
 vim.opt.number = true
@@ -11,7 +10,15 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.keymap.set('n', '<Space>', ':nohlsearch<CR>')
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.signcolumn = 'yes:1'
+--vim.cmd('hi SignColumn guibg=none')
+--vim.cmd('hi clear DiagnosticSignError')
+vim.api.nvim_set_hl(0, 'SignColumn', {bg='NONE'})
+--vim.api.nvim_set_hl(0, 'DiagnosticSignError', {bg='NONE'})
+
+vim.keymap.set('n', '<Space>', vim.cmd.nohlsearch)
 vim.keymap.set('n', '<C-left>', ':vertical resize -3<CR>')     --resize window
 vim.keymap.set('n', '<C-right>', ':vertical resize +3<CR>')
 vim.keymap.set('n', '<C-up>', ':resize +3<CR>')
@@ -25,6 +32,6 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
     pattern = "*.lua",
-    group = comment, 
+    group = comment,
     callback = function() vim.keymap.set('n', '<localleader>c', 'I--<esc>') end
 })
