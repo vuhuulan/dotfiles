7a54ba13a09c3483f204c6573b07c3ea4976c28a
vuhuulan <vuhuulan18@gmail.com>
2023-05-03 22:12:26 +0900

    nvim: Changed leader key to ',' and Telescope settings

--- nvim/lua/vhl/plugin_config/telescope.lua
+++ nvim/lua/vhl/plugin_config/telescope.lua
@@ -1,6 +1,6 @@
+vim.g.mapleader = ','
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<Leader><Leader>', builtin.oldfiles, {})
+vim.keymap.set('n', '<Leader>fp', builtin.find_files, {})
+vim.keymap.set('n', '<Leader>fo', builtin.oldfiles, {})
 vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
