6c2a0bff6901af3125bab09ac1ac53df524b30b1
vuhuulan <vuhuulan18@gmail.com>
2023-07-15 11:10:08 +0900

    nvim: Remap <C-L> to <Space>
    <C-L> currently does clear the search highlight, which in this commit
    changes to <Space> instead.

--- nvim/lua/vhl/set.lua
+++ nvim/lua/vhl/set.lua
@@ -14,9 +14,10 @@ vim.opt.signcolumn = 'yes:1'
 --vim.cmd('hi SignColumn guibg=none')
 --vim.cmd('hi clear DiagnosticSignError')
 --vim.api.nvim_set_hl(0, 'DiagnosticSignError', {bg='NONE'})
-vim.api.nvim_set_hl(0, 'SignColumn', {bg='NONE'})
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE' })
 
-vim.keymap.set('n', '<C-left>', ':vertical resize -3<CR>', opts)     --resize window
+vim.keymap.set('n', '<C-left>', ':vertical resize -3<CR>', opts) --resize window
 vim.keymap.set('n', '<C-right>', ':vertical resize +3<CR>', opts)
 vim.keymap.set('n', '<C-up>', ':resize +3<CR>', opts)
 vim.keymap.set('n', '<C-down>', ':resize -3<CR>', opts)
+vim.keymap.set('n', '<Space>', '<Cmd>nohlsearch|diffupdate|normal! <C-L><CR>', opts)
