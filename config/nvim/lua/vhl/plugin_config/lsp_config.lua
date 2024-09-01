f664065723b7711ae30ab197206185878b04a24f
vuhuulan <vuhuulan18@gmail.com>
2024-09-01 13:24:00 +0900

    nvim: Update pylsp setup

--- config/nvim/lua/vhl/plugin_config/lsp_config.lua
+++ config/nvim/lua/vhl/plugin_config/lsp_config.lua
@@ -87,11 +87,33 @@ require("lspconfig").pylsp.setup{
     settings = {
         pylsp = {
             plugins = {
-                pycodestyle = {
-                    ignore = {"E741"},
-                    maxLineLength = 100,
-                    hangClosing = false,
-                }
+                -- formatter options
+                black = { enabled = true },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+
+                -- linter options
+                pylint = { enabled = true, executable = "pylint" },
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+
+                -- type checker
+                pylsp_mypy = { enabled = true },
+
+                -- auto-completion options
+                jedi_completion = { fuzzy = true },
+
+                -- import sorting
+                pyls_isort = { enabled = true },
+                -- pycodestyle = {
+                --     ignore = {"E741"},
+                --     maxLineLength = 100,
+                --     hangClosing = false,
+                -- },
+                -- ruff = {
+                --     enable = true,
+                --     extendSelect = { "I" },
+                -- },
             }
         }
     }
