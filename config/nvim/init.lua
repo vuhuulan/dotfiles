c044229b30fbdae485b75fcc2a2e53e1361e54f2
vuhuulan <vuhuulan18@gmail.com>
2025-01-20 23:33:13 +0900

    nvim/migration: Testing lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,10 @@
-require("vhl.set")
-require("vhl.plugins")
-require("vhl.plugin_config")
+require("dev.lazy")
+
+vim.opt.relativenumber = true
+
+vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+vim.keymap.set('n', 'gri', vim.lsp.buf.implementation)
+vim.keymap.set('n', 'gO', vim.lsp.buf.document_symbol)
+vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+vim.keymap.set('v', 'gra', vim.lsp.buf.code_action)
