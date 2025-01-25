d8506f15bc7020ac4b97a1a3be2410d662e85577
vuhuulan <vuhuulan18@gmail.com>
2025-01-26 04:34:52 +0900

    nvim: Remove unused LSP

--- config/nvim/lua/configs/2_1_lsp.lua
+++ config/nvim/lua/configs/2_1_lsp.lua
@@ -8,7 +8,7 @@ return {
                     "williamboman/mason.nvim",
                 },
                 opts = {
-                    ensure_installed = { "pyright", "basedpyright" },
+                    ensure_installed = { "basedpyright" },
                 },
             },
             {
