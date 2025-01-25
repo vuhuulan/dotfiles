de06e78f5af9b29bed28837bcdf9cb20391a607b
vuhuulan <vuhuulan18@gmail.com>
2025-01-26 04:38:49 +0900

    nvim: Cleanup conform

--- config/nvim/lua/configs/2_0_conform.lua
+++ config/nvim/lua/configs/2_0_conform.lua
@@ -1,29 +1,23 @@
 return {
     {
-        "williamboman/mason.nvim",
-        opts = {},
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = { "ruff" },
-        },
-    },
-    {
         "stevearc/conform.nvim",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+            dependencies = { "williamboman/mason.nvim" },
+            opts = {
+                ensure_installed = { "ruff", "markdownlint-cli2" },
+            },
+        },
         opts = {
             formatters_by_ft = {
                 ["*"] = { "trim_whitespace", "trim_newlines" },
                 python = { "ruff_format" },
+                markdown = { "markdownlint-cli2" }
             },
             format_on_save = {
                 timeout_ms = 500,
                 lsp_fallback = true,
             },
         },
-        config = function(_, opts)
-            local conform = require("conform")
-            conform.setup(opts)
-        end
     },
 }
