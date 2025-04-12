5b63bd9d92df2e1b7baef24dd5718667a6c3f66a
vuhuulan <vuhuulan18@gmail.com>
2025-04-12 10:45:09 +0900

    chore: remove redundant keybinds since nvim 0.11 defaults them

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,13 +14,6 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.guicursor = ""
 
-vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
-vim.keymap.set('n', 'grr', vim.lsp.buf.references)
-vim.keymap.set('n', 'gri', vim.lsp.buf.implementation)
-vim.keymap.set('n', 'gO', vim.lsp.buf.document_symbol)
-vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
-vim.keymap.set('v', 'gra', vim.lsp.buf.code_action)
-
 -- f is for 'fuzzy find'
 nmap_leader('f/', '<Cmd>Pick history scope="/"<CR>', '"/" history')
 nmap_leader('f:', '<Cmd>Pick history scope=":"<CR>', '":" history')
