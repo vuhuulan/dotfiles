5dd604bae82fe6b472671f229a17dd140eab62b0
vuhuulan <vuhuulan18@gmail.com>
2025-05-05 19:21:44 +0900

    refactor(.config/nvim): cleanup colorscheme.lua

--- .config/nvim/lua/configs/colorscheme.lua
+++ .config/nvim/lua/configs/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
     {
         "folke/tokyonight.nvim",
-        -- "ellisonleao/gruvbox.nvim",
         lazy = false,
         priority = 1000,
-        -- opts = { contrast = "hard" },
         config = function()
             vim.cmd([[colorscheme tokyonight-storm]])
-            -- vim.cmd([[colorscheme gruvbox]])
         end,
     },
 }
