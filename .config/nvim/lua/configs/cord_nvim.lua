f26f39093751f1b32e5e7e9afb778311effb69ca
vuhuulan <vuhuulan18@gmail.com>
2025-05-25 21:31:32 +0900

    refactor(.config/nvim): remove cord due to security concern

--- .config/nvim/lua/configs/cord_nvim.lua
+++ .config/nvim/lua/configs/cord_nvim.lua
@@ -1,22 +1,22 @@
 return {
-    'vyfor/cord.nvim',
-    build = ':Cord update',
-    opts = {
-        editor = { tooltip = 'Neovim' },
-        display = {
-            swap_fields = true,
-        },
-        text = {
-            default = "",
-            viewing = "Viewing a file",
-            editing = "Editing a file",
-            file_browser = false,
-            plugin_manager = false,
-            lsp_manager = false,
-            docs = "Writing docs",
-            vcs = false,
-            notes = false,
-            workspace = false,
-        },
-    },
+    -- 'vyfor/cord.nvim',
+    -- build = ':Cord update',
+    -- opts = {
+    --     editor = { tooltip = 'Neovim' },
+    --     display = {
+    --         swap_fields = true,
+    --     },
+    --     text = {
+    --         default = "",
+    --         viewing = "Viewing a file",
+    --         editing = "Editing a file",
+    --         file_browser = false,
+    --         plugin_manager = false,
+    --         lsp_manager = false,
+    --         docs = "Writing docs",
+    --         vcs = false,
+    --         notes = false,
+    --         workspace = false,
+    --     },
+    -- },
 }
