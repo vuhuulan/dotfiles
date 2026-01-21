9a48c2a82e19332b838c33db1098b5d59d3f95e4
vuhuulan <vuhuulan18@gmail.com>
2026-01-21 17:36:41 +0900

    refactor(nvim): move all stray mason download into centralized module

--- .config/nvim/lua/configs/languages_config/lua.lua
+++ .config/nvim/lua/configs/languages_config/lua.lua
@@ -1,19 +1,11 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = { "lua-language-server" },
-        }
-    },
-    {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            },
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+        library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
     },
 }
